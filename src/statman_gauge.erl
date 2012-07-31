79f2aa7a36348ce462fd8e3f6b1c08b136a120ce
Knut Nesheim <knutin@gmail.com>
2012-07-31 11:53:17 +0200

    Expire unused gauges.

--- src/statman_gauge.erl
+++ src/statman_gauge.erl
@@ -1,5 +1,6 @@
 -module(statman_gauge).
--compile([export_all]).
+-export([init/0, set/2, expire/0, get_all/0]).
+-include_lib("eunit/include/eunit.hrl").
 
 -define(TABLE, statman_gauge).
 
@@ -8,8 +9,50 @@ init() ->
     ok.
 
 set(Key, Value) when is_integer(Value) orelse is_float(Value) ->
-    ets:insert(?TABLE, {Key, Value}),
+    set(Key, Value, now_to_seconds()).
+
+set(Key, Value, Timestamp) ->
+    ets:insert(?TABLE, {Key, Timestamp, Value}),
     ok.
 
+expire() ->
+    expire(now_to_seconds() - 60).
+
+expire(Threshold) ->
+    ets:select_delete(?TABLE, [{{'_', '$1', '_'}, [{'>', '$1', Threshold}], [true]}]).
+
 get_all() ->
-    ets:tab2list(?TABLE).
+    ets:select(?TABLE, [{ {'$1', '_', '$2'}, [], [{{'$1', '$2'}}]}]).
+
+now_to_seconds() ->
+    {MegaSeconds, Seconds, _} = os:timestamp(),
+    MegaSeconds * 1000000 + Seconds.
+
+
+%%
+%% TESTS
+%%
+
+gauge_test_() ->
+    {foreach,
+     fun setup/0, fun teardown/1,
+     [
+      ?_test(test_expire())
+     ]
+    }.
+
+setup() ->
+    init(),
+    [?TABLE].
+
+teardown(Tables) ->
+    lists:map(fun ets:delete/1, Tables).
+
+
+test_expire() ->
+    ?assertEqual([], get_all()),
+    set(foo, 30, now_to_seconds() - 3),
+    ?assertEqual([{foo, 30}], get_all()),
+    ?assertEqual(0, expire(now_to_seconds() - 0)),
+    ?assertEqual(1, expire(now_to_seconds() - 5)),
+    ?assertEqual([], get_all()).
