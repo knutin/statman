b2a10a923df466fc0f7e68d16a9eaaa29d579260
Knut Nesheim <knutin@gmail.com>
2013-07-16 15:54:07 +0200

    Bin microsecond timestamp values by default.

--- src/statman_histogram.erl
+++ src/statman_histogram.erl
@@ -29,11 +29,11 @@ init() ->
     ok.
 
 record_value(UserKey, {MegaSecs, Secs, MicroSecs}) when
-    is_integer(MegaSecs) andalso MegaSecs >= 0 andalso
-    is_integer(Secs) andalso Secs >=0 andalso
-    is_integer(MicroSecs) andalso MicroSecs >= 0 ->
+      is_integer(MegaSecs) andalso MegaSecs >= 0 andalso
+      is_integer(Secs) andalso Secs >=0 andalso
+      is_integer(MicroSecs) andalso MicroSecs >= 0 ->
     record_value(UserKey,
-        timer:now_diff(now(), {MegaSecs, Secs, MicroSecs}));
+                 bin(timer:now_diff(now(), {MegaSecs, Secs, MicroSecs})));
 
 record_value(UserKey, Value) when is_integer(Value) ->
     histogram_incr({UserKey, Value}, 1),
