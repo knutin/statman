2360353871589d5f3b0030231a9b349e30ec74a0
Knut Nesheim <knutin@gmail.com>
2013-09-17 14:23:04 +0200

    Poller now also handles histograms.

--- src/statman_poller.erl
+++ src/statman_poller.erl
@@ -5,8 +5,9 @@
 
 -export([start_link/0]).
 -export([add_gauge/1, add_gauge/2,
-         add_counter/1, add_counter/2]).
--export([remove_gauge/1, remove_counter/1]).
+         add_counter/1, add_counter/2,
+         add_histogram/1, add_histogram/2]).
+-export([remove_gauge/1, remove_counter/1, remove_histogram/1]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
@@ -20,15 +21,16 @@
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-add_gauge(F)             -> add_fun({gauge, F}, 10000).
-add_gauge(F, Interval)   -> add_fun({gauge, F}, Interval).
+add_gauge(F)               -> add_fun({gauge, F}, 10000).
+add_gauge(F, Interval)     -> add_fun({gauge, F}, Interval).
+add_counter(F)             -> add_fun({counter, F}, 10000).
+add_counter(F, Interval)   -> add_fun({counter, F}, Interval).
+add_histogram(F)           -> add_fun({histogram, F}, 10000).
+add_histogram(F, Interval) -> add_fun({histogram, F}, Interval).
 
-add_counter(F)           -> add_fun({counter, F}, 10000).
-add_counter(F, Interval) -> add_fun({counter, F}, Interval).
-
-
-remove_gauge(F)   -> remove_fun({gauge, F}).
-remove_counter(F) -> remove_fun({counter, F}).
+remove_gauge(F)     -> remove_fun({gauge, F}).
+remove_counter(F)   -> remove_fun({counter, F}).
+remove_histogram(F) -> remove_fun({histogram, F}).
 
 remove_fun(TypedF) -> gen_server:call(?MODULE, {remove, TypedF}).
 
@@ -62,6 +64,8 @@ handle_info({poll, Interval}, State) ->
                           [statman_gauge:set(K, V) || {K, V} <- F()];
                       ({I, {counter, F}}) when I =:= Interval ->
                           [statman_counter:incr(K, V) || {K, V} <- F()];
+                      ({I, {histogram, F}}) when I =:= Interval ->
+                          [statman_histogram:record_value(K, V) || {K, V} <- F()];
                       (_) ->
                           ok
                   end, State#state.fs),
@@ -92,25 +96,31 @@ poller_test_() ->
 
 setup() ->
     statman_counter:init(),
-    statman_gauge:init().
+    statman_gauge:init(),
+    statman_histogram:init().
 
 teardown(_) ->
-    [ets:delete(T) || T <- [statman_counters, statman_gauges]].
+    [ets:delete(T) || T <- [statman_counters, statman_gauges, statman_histograms]].
 
 poller_fun() ->
-    GaugeF = fun() -> [{gauge, 5}] end,
-    CounterF = fun () -> [{counter, 5}] end,
+    GaugeF     = fun() -> [{gauge, 5}] end,
+    CounterF   = fun () -> [{counter, 5}] end,
+    HistogramF = fun () -> [{histogram, 5}, {histogram, 10}] end,
 
     ?assertEqual([], statman_counter:get_all()),
     ?assertEqual([], statman_gauge:get_all()),
+    ?assertEqual([], statman_histogram:keys()),
 
     {ok, _} = start_link(),
     ok = add_counter(CounterF, 100),
     ok = add_gauge(GaugeF, 100),
-    timer:sleep(100),
+    ok = add_histogram(HistogramF, 100),
+    timer:sleep(200),
 
-    ?assertEqual([{counter, 5}], statman_counter:get_all()),
-    ?assertEqual([{gauge, 5}], statman_gauge:get_all()).
+    ?assertEqual([histogram], statman_histogram:keys()),
+    ?assertEqual([counter], statman_counter:counters()),
+    ?assertMatch([{gauge, _}], statman_gauge:get_all()).
+
 
 remove_poller() ->
     GaugeF = fun() -> [{gauge, 5}] end,
