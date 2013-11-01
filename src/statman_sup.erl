d913c6aa238da3af29bcbdbddd910cfab071bdf2
Martin Wiso <martin@wiso.cz>
2013-11-01 17:49:14 +0100

    Refactor supervisor to preserve cluster design with on demand aggregator

--- src/statman_sup.erl
+++ src/statman_sup.erl
@@ -1,19 +1,19 @@
 -module(statman_sup).
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/1]).
 -export([init/1]).
 
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type, Args),
+        {I, {I, start_link, Args}, permanent, 5000, Type, [I]}).
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(ReportInterval) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, [ReportInterval]).
 
-init([]) ->
+init(ReportInterval) ->
     Children = [
-                ?CHILD(statman_server, worker),
-                ?CHILD(statman_aggregator, worker),
-                ?CHILD(statman_poller, worker),
-                ?CHILD(statman_poller_registry, worker)
+                ?CHILD(statman_server, worker, [ReportInterval]),
+                ?CHILD(statman_poller, worker, []),
+                ?CHILD(statman_poller_registry, worker, [])
                ],
     {ok, {{one_for_one, 5, 10}, Children}}.
